2dc43068736c6db9f6868e710e44b5e3e046d9ce
tylersisia <tyler@pdssystems.com>
2020-01-04 14:52:30 -0500

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,5 @@
+<?php
+
+// used for API calls change it to your own Qwertycoin RPC node
+// daemon:port 8197 is the default RPC Port
+$apiNode = 'http://127.0.0.1:8197';
---
+++ config.js
@@ -0,0 +1,24 @@
+var api = "127.0.0.1:8197";
+var apiList = [
+	"http://chimera.pdss2access.com:8117"
+];
+var blockTargetInterval = 60;
+var coinUnits = 100;
+var symbol = 'CMRA';
+var refreshDelay = 20000;
+// pools stats by MainCoins
+var poolsStat =
+	[
+		["chimera.pdss2access.com","http://chimera.pdss2access.com:8117/stats"],
+		["54.172.240.25","http://54.172.240.25:8117/stats"],
+		["cmra.miningpool.fun", "https://cmra.miningpool.fun:8151/stats"],
+		["cryptonote.club","https://mng.cryptonote.club:8197/stats"],
+		["cryptonote.club 2","https://btch.cryptonote.club:8197/stats"],
+		["semipool.com","https://webcmra.semipool.com/api"],
+		["minercountry.com","https://wtips.minercountry.com/stats"]
+    ];
+var nodesStat =
+	[
+		["us-east.chimeraproject.io:8197"],
+		["cryptodude.chimeraproject.io:8197"]
+    ];
