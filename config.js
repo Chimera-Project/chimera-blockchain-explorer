f53c50988cb7d1e93d2579b557b27f6b7a2bbf28
tylersisia <tyler@pdssystems.com>
2020-01-04 17:30:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var poolsStat =
     ];
 var nodesStat =
 	[
-		["us-east.chimeraproject.io:8197"],
+		["us-east.chimeraproject.io:11246"],
 		["explorer.chimeraproject.io:8197"],
-		["cryptodude.chimeraproject.io:8197"]
+		["cryptodude.chimeraproject.io:11246"]
     ];
