2823523a76496cd04cec50759d6642093411edd0
tylersisia <tyler@pdssystems.com>
2020-01-04 17:52:40 -0500

    Update config.php

--- api/config.php
+++ api/config.php
@@ -2,4 +2,4 @@
 
 // used for API calls change it to your own Qwertycoin RPC node
 // daemon:port 8197 is the default RPC Port
-$apiNode = 'http://172.26.10.255:8197';
+$apiNode = 'http://54.172.240.25:8117';
